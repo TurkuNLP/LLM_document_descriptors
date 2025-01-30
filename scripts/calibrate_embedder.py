bd28dbca60432d56909c89d24759901c69c29bd1
Otto Tarkka <otto.tarkka@gmail.com>
2025-01-30 14:00:44 +0200

    Add embedding calibrator script.

---
+++ scripts/calibrate_embedder.py
@@ -0,0 +1,31 @@
+from embed import StellaEmbedder
+from datasets import load_dataset
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+
+
+data = load_dataset("ryanbaker/synonyms_1K")
+embedder = StellaEmbedder()
+
+
+min_similarities = []
+max_similarities = []
+mean_similarities = []
+for syn_list in data["train"]["synonyms"]:
+    embeddings = embedder.embed_descriptors(syn_list)
+    similarity_matrix = cosine_similarity(embeddings)
+    # Mask the diagonal elements
+    masked_similarity_matrix = np.ma.masked_where(np.eye(similarity_matrix.shape[0]), similarity_matrix)
+
+    # Calculate average, minimum, and maximum cosine similarity ignoring identity matches
+    mean_similarities.append(np.mean(masked_similarity_matrix))
+    min_similarities.append(np.min(masked_similarity_matrix))
+    max_similarities.append(np.max(masked_similarity_matrix))
+
+print("="*20)
+print(f"Average Cosine Similarity: {np.mean(mean_similarities):.4f}")
+print(f"Minimum Cosine Similarity: {np.mean(min_similarities):.4f}")
+print(f"Maximum Cosine Similarity: {np.mean(max_similarities):.4f}")
+print("="*20)
+    
