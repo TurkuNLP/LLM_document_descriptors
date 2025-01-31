1c98f89732f51bfd45f75ccb9a52863c5f08f058
Otto Tarkka <otto.tarkka@gmail.com>
2025-01-31 10:52:13 +0200

    Update.

--- scripts/run_vllm.sh
+++ scripts/run_vllm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=vllm_inference
 #SBATCH --account=project_462000353
-#SBATCH --partition=standard-g
-#SBATCH --time=12:00:00
+#SBATCH --partition=dev-g
+#SBATCH --time=00:59:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=15
@@ -27,7 +27,7 @@ PYTORCH_HIP_ALLOC_CONF=expandable_segments:True,garbage_collection_threshold:0.8
 
 gpu-energy --save
 
-srun python3 vllm_document_descriptors.py --run-id="70B_3.3_0" \
+srun python3 vllm_document_descriptors.py --run-id="70B_3.3_0-test0" \
                                           --temperature=0.1 \
                                           --batch-size=50 \
                                           --num-batches=20 \
