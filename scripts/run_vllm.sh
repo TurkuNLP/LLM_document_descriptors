00718cc08999fefdd430ad4ddb556550b1033d4e
Otto Tarkka <otto.tarkka@gmail.com>
2025-06-27 11:15:59 +0300

    Update.

--- scripts/run_vllm.sh
+++ scripts/run_vllm.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=vllm_inference
 #SBATCH --account=project_462000353
 #SBATCH --partition=standard-g
-#SBATCH --time=1-00:00:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=15
@@ -24,16 +24,15 @@ PYTORCH_HIP_ALLOC_CONF=expandable_segments:True,garbage_collection_threshold:0.8
 
 gpu-energy --save
 
-run_id="new_descriptors_8"
+run_id="test1"
+
 
 srun python3 ../doc_descriptors/doc_descriptors_with_explainers.py --run-id=$run_id \
                                                                    --temperature=0.1 \
-                                                                   --batch-size=200 \
-                                                                   --num-batches=40 \
+                                                                   --batch-size=20 \
+                                                                   --num-batches=10 \
                                                                    --num-rewrites=3 \
-                                                                   --start-index=72000 \
-                                                                   --max-vocab=0 \
-                                                                   --synonym-threshold=0.3 \
+                                                                   --start-index=0 \
                                                                    --data-source="fineweb" \
                                                                    --checkpoint-interval=50 \
 
