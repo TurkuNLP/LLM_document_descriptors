dd0805e3235550715753b0af8cb354469552f237
Otto Tarkka <otto.tarkka@gmail.com>
2025-12-09 15:37:39 +0200

    Harmonization scripts

---
+++ harmonize/run_harmonize_with_schema.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=harmonize
+#SBATCH --account=project_462000963
+#SBATCH --partition=standard-g
+#SBATCH --time=23:55:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=8
+#SBATCH --cpus-per-task=15
+#SBATCH --mem=128G
+#SBATCH -o ../logs/%j.out
+#SBATCH -e ../logs/%j.err
+
+module purge
+module use /appl/local/csc/modulefiles
+module load pytorch/2.5
+
+source .venv_pt2.5_merge/bin/activate
+
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
+
+gpu-energy --save
+
+run_id="harmonize_ag_news"
+
+srun python3 harmonize_with_schema.py --run-id=$run_id \
+                                      --input="../results/benchmarks/ag_news/descriptors_ag_news.jsonl" \
+                                      --schema="../results/schema.jsonl"
+
+gpu-energy --diff
